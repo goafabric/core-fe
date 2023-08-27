8a10d1fc6a9384d1ccde056aaa75f5a00a20b116
Andreas Mautsch <amautsch>
2023-08-27 11:38:27 +0200

    changes

--- src/burger/BurgerMenu.js
+++ src/burger/BurgerMenu.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './BurgerMenu.css';
+import CatalogDataTable from './CatalogDataTable'; // Import the new component
 
 const BurgerMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -64,35 +65,14 @@ const BurgerMenu = () => {
           ))}
         </ul>
       </div>
-      <div className="search-field">
-        <input
-          type="text"
-          placeholder="Search..."
-          value={searchText}
-          onChange={handleSearchChange}
-        />
-      </div>
+
       {selectedItem && (
-        <div className="table">
-          <table>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Display</th>
-                <th>Code</th>
-              </tr>
-            </thead>
-            <tbody>
-              {apiData.map((item) => (
-                <tr key={item.id}>
-                  <td>{item.id}</td>
-                  <td>{item.display}</td>
-                  <td>{item.code}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+        <CatalogDataTable
+          selectedItem={selectedItem}
+          searchText={searchText}
+          handleSearchChange={handleSearchChange}
+          apiData={apiData}
+        />
       )}
     </div>
   );

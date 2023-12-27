c5e2b211631608b45e76150e7037b0fbcdcc9593
Andreas Mautsch <amautsch>
2023-12-27 12:22:21 +0100

    changes

--- catalog-fe/public/catalogs/properties.js
+++ catalog-fe/public/catalogs/properties.js
@@ -1,2 +1,3 @@
 const devMode = true;
 const serviceUrl = devMode == true ? 'http://localhost:50600' : '/catalogs';
+const mockData = [{"id": "10226310-3ed0-448d-8d8b-0cedd716c3e4", "version": 0, "code": "GOAE1", "display": "Normale Untersuchung", "price": 4.51}, {"id": "fd5e8550-2edf-4471-9485-1a1604b1ad0a", "version": 0, "code": "GOAE2", "display": "Umfassende Untersuchung", "price": 9.21}, {"id": "4ae062bd-1e20-49d6-a213-df7df0feae25", "version": 0, "code": "GOAE3", "display": "Verband angelegt", "price": 3.21}, {"id": "4df94a1e-c182-4332-9fa3-b52367d8c8b4", "version": 0, "code": "GOAE4", "display": "Psychotherapie", "price": 100.21}];

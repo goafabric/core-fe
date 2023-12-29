ba8bff4b634e6daeccc22109cd75c89c38219f9e
Andreas Mautsch <amautsch>
2023-12-29 10:59:38 +0100

    changes

--- catalog-fe/build.sh
+++ catalog-fe/build.sh
@@ -1,7 +1,5 @@
 IMAGE_NAME=goafabric/core-fe:1.0.0-SNAPSHOT
 
-docker buildx create --name mybuilder --use && docker buildx build --platform linux/amd64,linux/arm64 -t $IMAGE_NAME .
-#&& docker buildx stop mybuilder && docker buildx rm mybuilder
-#docker build -t $IMAGE_NAME .
-docker push $IMAGE_NAME
+docker buildx create --name mybuilder --use && docker buildx build --platform linux/amd64,linux/arm64 -t $IMAGE_NAME --push . ; docker buildx stop mybuilder && docker buildx rm mybuilder
+#docker build -t $IMAGE_NAME . && docker push $IMAGE_NAME
 docker run --pull always --rm -p 8081:80 $IMAGE_NAME

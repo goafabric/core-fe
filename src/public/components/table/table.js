57c872e65dc6e8df64caab2b045ff47a10591d7f
Andreas Mautsch <amautsch>
2024-01-07 12:54:44 +0100

    ref

--- src/public/components/table/table.js
+++ src/public/components/table/table.js
@@ -1,4 +1,5 @@
 import * as properties from "../../properties.js";
+import { addCellListener } from "./tableCellEditor.js";
 
 let searchUrl = "";
 let tableColumns = [];
@@ -8,51 +9,29 @@ export function setSearchUrl(url, columns, callback) {
     searchUrl = url;
     tableColumns = columns;
     bodyCallbackFunction = callback;
+    addCellListener();
     performSearch();
-
-    document.getElementById('table').addEventListener('dblclick', function(event) {
-        const target = event.target;
-        if (target.tagName === 'TD') {
-            enableEdit(target);
-        }
-    });
 }
 
 export function performSearch() {
+    const url = searchUrl + document.getElementById('search').value;
     updateHeader();
     if (properties.mockMode == true) {
         updateTableBody(properties.mockData);
     } else {
-        const url = searchUrl + document.getElementById('search').value;
         fetch(url).then(response => response.json()).then(data => updateTableBody(data));
     }
+}
+
+function updateHeader() {
+    document.getElementById('tableHeader').innerHTML =
+        tableColumns.map(column => '<th>' + column.toUpperCase() + '</th>').join('');
 }
 
 function updateTableBody(data) {
     if (searchUrl.includes("/encounters")) { data = data[0].medicalRecords; }
     document.getElementById('tableBody').innerHTML =
         data.map(arElement => '<tr>' + bodyCallbackFunction(arElement) + '</tr>').join('');
-}
-
-function updateHeader() {
-    const header = tableColumns.map(column => '<th>' + column.toUpperCase() + '</th>').join('');
-    document.getElementById('tableHeader').innerHTML = header;
-}
-
-function enableEdit(cell) {
-    var inputElement = document.createElement('input');
-    inputElement.value = cell.innerText.trim();
-    cell.innerHTML = '';
-    cell.appendChild(inputElement);
-    inputElement.focus();
-    inputElement.addEventListener('blur', () => saveEdit(cell, inputElement.value));
-    inputElement.addEventListener('keyup', event => event.key === 'Enter' && saveEdit(cell, inputElement.value));
-}
-
-function saveEdit(cell, newValue) {
-    cell.innerHTML = newValue;
-    var cellValues = Array.from(cell.parentNode.cells).map(cell => cell.innerText.trim());
-    console.log('Cell values of the current row:', cellValues);
 }
 
 document.addEventListener('DOMContentLoaded', () => document.getElementById('tab1').click());
---
+++ src/public/components/table/tableCellEditor.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => document.getElementById('tab1').click());
+
+export function addCellListener() {
+    document.getElementById('table').addEventListener('dblclick', function(event) {
+        const target = event.target;
+        if (target.tagName === 'TD') {
+            enableEdit(target);
+        }
+    });
+}
+
+function enableEdit(cell) {
+    var inputElement = document.createElement('input');
+    inputElement.value = cell.innerText.trim();
+    cell.innerHTML = '';
+    cell.appendChild(inputElement);
+    inputElement.focus();
+    inputElement.addEventListener('blur', () => saveEdit(cell, inputElement.value));
+    inputElement.addEventListener('keyup', event => event.key === 'Enter' && saveEdit(cell, inputElement.value));
+}
+
+function saveEdit(cell, newValue) {
+    cell.innerHTML = newValue;
+    var cellValues = Array.from(cell.parentNode.cells).map(cell => cell.innerText.trim());
+    console.log('Cell values of the current row:', cellValues);
+}

e086eb408367ad647071e54571b7350e57e69242
Andreas Mautsch <amautsch>
2024-01-07 09:55:34 +0100

    ref

--- src/public/catalogs/callback.js
+++ src/public/catalogs/callback.js
@@ -1,2 +1,5 @@
 import { setSearchUrl } from "../components/table/table.js";
-export function search(url, columns) { setSearchUrl(catalogServiceUrl + url, columns) }
+
+document.getElementById('tab1').addEventListener('click', setSearchUrl(catalogServiceUrl + '/chargeitems/findByDisplay?display=',['code', 'display', 'price']));
+document.getElementById('tab2').addEventListener('click', setSearchUrl(catalogServiceUrl + '/diagnosis/findByDisplay?display=',['code', 'display', 'price']));
+document.getElementById('tab3').addEventListener('click', setSearchUrl(catalogServiceUrl + '/insurances/findByDisplay?display=',['code', 'display', 'price']));

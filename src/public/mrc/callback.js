6c133d3ee5334cb8b070e564b10de026f34106b5
Andreas Mautsch <amautsch>
2024-01-07 10:59:41 +0100

    ref

---
+++ src/public/mrc/callback.js
@@ -0,0 +1,18 @@
+import { setSearchUrl } from "../components/table/table.js";
+import * as properties from "../properties.js";
+
+document.getElementById('tab1').addEventListener('click', () =>
+    search(['givenName', 'familyName', 'address[0].street', 'address[0].city']));
+
+function search(columns) {
+    if (properties.mockMode == true) {
+        setSearchUrl(properties.coreServiceUrl + '/encounters/findByPatientIdAndDisplay?patientId=', columns);
+        return;
+    }
+
+    fetch(properties.coreServiceUrl + '/patients/findByFamilyName?familyName=Burns').then(response => response.json())
+    .then(data => {
+        var url = '/encounters/findByPatientIdAndDisplay?patientId=' + data[0].id + '&display=';
+        setSearchUrl(properties.coreServiceUrl + url, columns);
+    });
+}

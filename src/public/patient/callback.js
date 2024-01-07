268797ae1c9a7dc65b3a6c8c2086d817eb9a41d2
Andreas Mautsch <amautsch>
2024-01-07 10:51:58 +0100

    ref

--- src/public/patient/callback.js
+++ src/public/patient/callback.js
@@ -2,5 +2,5 @@ import { setSearchUrl } from "../components/table/table.js";
 import * as properties from "../properties.js";
 
 document.getElementById('tab1').addEventListener('click', function() {
-  setSearchUrl(properties.catalogServiceUrl + '/patients/findByGivenName?givenName=',['givenName', 'familyName', 'address[0].street', 'address[0].city']);
+  setSearchUrl(properties.coreServiceUrl + '/patients/findByGivenName?givenName=',['givenName', 'familyName', 'address[0].street', 'address[0].city']);
 });

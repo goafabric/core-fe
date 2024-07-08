a48df63b7d812645455b327f52d3876b36f753f5
Andreas Mautsch <amautsch>
2024-07-08 13:29:42 +0200

    react removed

--- src/public/home/callback.js
+++ src/public/home/callback.js
@@ -1,11 +1,11 @@
-import * as properties from "../../properties.js";
+ import * as properties from "../properties.js";
 
 document.addEventListener("DOMContentLoaded", function() {
     fetch(properties.coreServiceUrl + '/users/getUserInfo')
         .then(response => response.json())
         .then(data => {
             const userInfo = document.getElementById('user-info');
-            userInfo.innerHTML = `Welcome ${data.userName} for tenant ${data.tenantId}`;
+            userInfo.innerHTML = `Hello ${data.userName} from Tenant ${data.tenantId}`;
         })
         .catch(error => {
             console.error('Error fetching user info:', error);
